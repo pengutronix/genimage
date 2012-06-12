c850c8b4047b8b7bcda0076ce1207018007bb795
Michael Olbrich <m.olbrich@pengutronix.de>
2012-06-12 09:48:05 +0200

    build with autotools
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- genimage.c
+++ genimage.c
@@ -15,7 +15,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <confuse.h>
 #include <stdlib.h>
--- image-ext2.c
+++ image-ext2.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <confuse.h>
 #include <stdio.h>
 #include <string.h>
