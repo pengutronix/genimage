pengutronix/genimage

commit adf18f0770105ad78da7073b5fabaae54997a988

image-flash: Pad empty flash partitions to size
According to the documentation a flash image is
> the partition contents padded to the partition sizes concatenated together

Currently empty partitions are not padded to size, resulting in overall
size not equal to the sum of all partition sizes if the last partition
is empty.

Signed-off-by: Tobias Schramm <tobleminer@gmail.com>

image-flash.c
@@ -50,15 +50,12 @@ static int flash_generate(struct image *image)
 		}
 		mode = MODE_APPEND;
 
-		if (!part->image)
-			continue;
-
-		child = image_get(part->image);
-		if (!child) {
-			image_error(image, "could not find %s\n", part->name);
-			return -EINVAL;
+		if (part->image) {
+			child = image_get(part->image);
+			infile = imageoutfile(child);
+		} else {
+			infile = NULL;
 		}
-		infile = imageoutfile(child);
 
 		ret = pad_file(image, infile, part->size, 0xFF, mode);
 		if (ret) {

commit 408ce21d15b5a0071bca553a21235611ebdbe194

image-flash: fix regression with empty partitions
adf18f077010 ("image-flash: Pad empty flash partitions to size") causes
a regression in image-flash.c
Due to a difference in handling of the `size` argument by `pad_file`
depending on `infile == NULL` adf18f077010 is incorrect.
This patch changes the code to call `pad_file` differently if
`infile == NULL`

Fixes: adf18f077010 ("image-flash: Pad empty flash partitions to size")
Signed-off-by: Tobias Schramm <t.schramm@manjaro.org>

image-flash.c
@@ -53,11 +53,11 @@ static int flash_generate(struct image *image)
 		if (part->image) {
 			child = image_get(part->image);
 			infile = imageoutfile(child);
+			ret = pad_file(image, infile, part->size, 0xFF, mode);
 		} else {
-			infile = NULL;
+			ret = pad_file(image, NULL, part->offset + part->size, 0xFF, mode);
 		}
 
-		ret = pad_file(image, infile, part->size, 0xFF, mode);
 		if (ret) {
 			image_error(image, "failed to write image partition '%s'\n",
 					part->name);