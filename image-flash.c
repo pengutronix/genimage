pengutronix/genimage

commit adf18f0770105ad78da7073b5fabaae54997a988

image-flash: Pad empty flash partitions to size
According to the documentation a flash image is
> the partition contents padded to the partition sizes concatenated together

Currently empty partitions are not padded to size, resulting in overall
size not equal to the sum of all partition sizes if the last partition
is empty.

Signed-off-by: Tobias Schramm <tobleminer@gmail.com>

image-flash.c
@@ -50,15 +50,12 @@ static int flash_generate(struct image *image)
 		}
 		mode = MODE_APPEND;
 
-		if (!part->image)
-			continue;
-
-		child = image_get(part->image);
-		if (!child) {
-			image_error(image, "could not find %s\n", part->name);
-			return -EINVAL;
+		if (part->image) {
+			child = image_get(part->image);
+			infile = imageoutfile(child);
+		} else {
+			infile = NULL;
 		}
-		infile = imageoutfile(child);
 
 		ret = pad_file(image, infile, part->size, 0xFF, mode);
 		if (ret) {

commit e17fdfb8137f3397bdcf3edd5a33daeba88986c9

util: pad_file: always use the full image size for the size argument
Before this, the interpretation of the 'size' argument depended on the
'infile'. If no 'infile' was given, then 'size' was the final size of the
total image.
If an 'infile' was given, then 'size' was the number of bytes to add to the
images.

This is confusing, so change it to always mean the full size the image is
supposed to be once pad_file() is done.

Fixes: adf18f077010 ("image-flash: Pad empty flash partitions to size")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

image-flash.c
@@ -57,7 +57,7 @@ static int flash_generate(struct image *image)
 			infile = NULL;
 		}
 
-		ret = pad_file(image, infile, part->size, 0xFF, mode);
+		ret = pad_file(image, infile, part->offset + part->size, 0xFF, mode);
 		if (ret) {
 			image_error(image, "failed to write image partition '%s'\n",
 					part->name);

image-hd.c
@@ -428,7 +428,7 @@ static int hdimage_generate(struct image *image)
 		child = image_get(part->image);
 		infile = imageoutfile(child);
 
-		ret = pad_file(image, infile, child->size, 0x0, MODE_APPEND);
+		ret = pad_file(image, infile, part->offset + child->size, 0x0, MODE_APPEND);
 
 		if (ret) {
 			image_error(image, "failed to write image partition '%s'\n",

util.c
@@ -462,15 +462,14 @@ int pad_file(struct image *image, const char *infile,
 
 	buf = xzalloc(4096);
 
-	if (!infile) {
-		if ((unsigned long long)s.st_size > size) {
-			image_error(image, "output file '%s' too large\n", outfile);
-			ret = -EINVAL;
-			goto err_out;
-		}
-		size = size - s.st_size;
-		goto fill;
+	if ((unsigned long long)s.st_size > size) {
+		image_error(image, "output file '%s' too large\n", outfile);
+		ret = -EINVAL;
+		goto err_out;
 	}
+	size = size - s.st_size;
+	if (!infile)
+		goto fill;
 
 	if ((s.st_mode & S_IFMT) == S_IFREG) {
 		ret = map_file_extents(image, infile, f, size, &extents, &extent_count);