pengutronix/genimage

commit 1c530126810655b645951da50a8f1863312419b0

util: systemp(): use shell defined in SHELL rather than /bin/sh
This patch changes systemp() to use the shell defined in the environment
variable SHELL rather then hard coded /bin/sh, so that the called
programs can use shell functions exported in the calling shell. Use
"/bin/sh" as a fall back.

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

util.c
@@ -216,6 +216,7 @@ int systemp(struct image *image, const char *fmt, ...)
 	pid = fork();
 
 	if (!pid) {
+		const char *shell;
 		int fd;
 
 		if (loglevel() < 1) {
@@ -230,7 +231,11 @@ int systemp(struct image *image, const char *fmt, ...)
 			dup2(STDERR_FILENO, STDOUT_FILENO);
 		}
 
-		ret = execl("/bin/sh", "sh", "-c", buf, NULL);
+		shell = getenv("SHELL");
+		if (!shell || shell[0] == 0x0)
+			shell = "/bin/sh";
+
+		ret = execl(shell, shell, "-c", buf, NULL);
 		if (ret < 0) {
 			ret = -errno;
 			error("Cannot execute %s: %s\n", buf, strerror(errno));

commit 1e9b9be0bb4c89b849957b1fc6263532c00d4b44

util: systemp(): don't use shell defined in SHELL, but GENIMAGE_SHELL
As Matthias Schiffer (@schiffermtq) pointed out, using SHELL instead
of "/bin/sh" breaks the test suite if not using a POSIX compatible
shell like zsh. Fix this problem by not using blindly SHELL, but
GENIMAGE_SHELL. If this environment variable is set, we assume the
user knows what she's doing.

Link: https://github.com/pengutronix/genimage/pull/161#issuecomment-891761302
Fixes: 1c5301268106 ("util: systemp(): use shell defined in SHELL rather than /bin/sh")
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

util.c
@@ -231,7 +231,7 @@ int systemp(struct image *image, const char *fmt, ...)
 			dup2(STDERR_FILENO, STDOUT_FILENO);
 		}
 
-		shell = getenv("SHELL");
+		shell = getenv("GENIMAGE_SHELL");
 		if (!shell || shell[0] == 0x0)
 			shell = "/bin/sh";
 