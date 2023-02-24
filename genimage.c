e123753a4f03415dad7c370bc8c15be5aa74b726
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2023-02-24 11:52:22 +0100

    image-hd: implement "fill" option for partitions
    When bootstrapping, it can be useful to ensure that a partition has
    completely known contents, especially when the partition just contains
    raw binary data and not a filesystem.
    
    When generating an image file, this is implicitly done already because
    the non-written parts of the partition(s) are implicitly zero-filled
    when the containing image file is extended to its final size.
    
    But when using genimage to directly populate a block device, no such
    implicit zero-filling is done.
    
    Add a "fill" boolean option, and use that to decide between passing
    the partition's size or the child image's size to insert_image().
    
    Signed-off-by: Rasmus Villemoes <rasmus.villemoes@prevas.dk>

--- genimage.c
+++ genimage.c
@@ -99,6 +99,7 @@ static cfg_opt_t partition_opts[] = {
 	CFG_BOOL("read-only", cfg_false, CFGF_NONE),
 	CFG_BOOL("hidden", cfg_false, CFGF_NONE),
 	CFG_BOOL("no-automount", cfg_false, CFGF_NONE),
+	CFG_BOOL("fill", cfg_false, CFGF_NONE),
 	CFG_STR("image", NULL, CFGF_NONE),
 	CFG_STR_LIST("holes", NULL, CFGF_NONE),
 	CFG_BOOL("autoresize", 0, CFGF_NONE),
@@ -398,6 +399,7 @@ static int parse_partitions(struct image *image, cfg_t *imagesec)
 		part->read_only = cfg_getbool(partsec, "read-only");
 		part->hidden = cfg_getbool(partsec, "hidden");
 		part->no_automount = cfg_getbool(partsec, "no-automount");
+		part->fill = cfg_getbool(partsec, "fill");
 		part->image = cfg_getstr(partsec, "image");
 		part->autoresize = cfg_getbool(partsec, "autoresize");
 		part->in_partition_table = cfg_getbool(partsec, "in-partition-table");
